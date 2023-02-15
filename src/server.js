e9cf60080f451678b3d6d80b3b0e6a99b1d54dc7
Louis Gadza <siuolgadza@gmail.com>
2023-02-15 15:31:59 +0100

    well

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { newConnectionHandler } from "./socket/index.js";
+
+const expressServer = express();
+const port = process.env.PORT || 3001;
+
+// ************************************ SOCKET.IO ********************************
+const httpServer = createServer(expressServer);
+const io = new Server(httpServer);
+
+io.on("connection", newConnectionHandler);
+
+// *********************************** MIDDLEWARES *******************************
+
+// ************************************ ENDPOINTS ********************************
+
+// *********************************** ERROR HANDLERS ****************************
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  httpServer.listen(port, () => {
+    console.table(listEndpoints(expressServer));
+    console.log(`Server is running on port ${port}`);
+  });
+});
